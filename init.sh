1e105aef0de6f0ebfa783af7b56d27c3f32e5fc0
Zl0n <zl0n@wtf.com>
2022-06-03 20:20:29 +0300

    add init.sh v1

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 # скачиваем необходимые зависимости
-sudo apt-get install wget jq htop ncdu tmux mc git-core -y
+sudo apt-get install wget jq tmux ncdu htop git-core -y
 
 # скачиваем исполняемые файлы одной командой
 mkdir $HOME/subspace; \
@@ -15,17 +15,19 @@ sudo mv * /usr/local/bin/ && \
 cd $HOME && \
 rm -Rvf $HOME/subspace
 
+# проверяем версии бинарников
+echo -e "\n$(farmer --version)\n$(subspace --version)\n"
+
 # фиксим журнал
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
 EOF
-
 
 # рестартим журнал
 sudo systemctl restart systemd-journald
 
 cd ~
 git clone https://github.com/zl0n/subsp.git
-cd ./subsp
+cd ~/subsp
 
 exit
